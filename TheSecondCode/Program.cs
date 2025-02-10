049f10d0aa1f8449ebd35a72351376706677bb07
JasonParzival <ruslanmuhamed123@gmail.com>
2025-02-10 22:16:36 +0800

    Добавьте файлы проекта.

---
+++ TheSecondCode/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace TheSecondCode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var smth = 124345;
+            string str = smth.ToString();
+            bool forif = true;
+            for (int i = 1; i < str.Length; i++)
+            {
+                if ((int)Char.GetNumericValue(str[i - 1]) < (int)Char.GetNumericValue(str[i]))
+                {
+                    forif = true;
+                }
+                else
+                {
+                    forif = false;
+                    break;
+                }
+            }
+            if (forif)
+            {
+                Console.WriteLine("Последовательность: " + str + " положительна!");
+            }
+            else
+            {
+                Console.WriteLine("Последовательность: " + str + " отрицательна!");
+            }
+        }
+    }
+}
