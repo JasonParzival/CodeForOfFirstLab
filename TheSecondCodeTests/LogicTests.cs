cc1e3e292139b474231b8032e2e52cfd22c24223
JasonParzival <ruslanmuhamed123@gmail.com>
2025-02-13 20:55:54 +0800

    Добавил тесты

---
+++ TheSecondCodeTests/LogicTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TheSecondCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheSecondCode.Tests
+{
+    [TestClass()]
+    public class LogicTests
+    {
+        [TestMethod()]
+        public void PositiveResultTest()
+        {
+            var smth = 12345;
+
+            string message = Logic.Message(smth);
+
+            Assert.AreEqual("Последовательность: " + smth + " положительна!", message);
+        }
+
+        [TestMethod()]
+        public void NegativeResultTest()
+        {
+            var smth = 1423845;
+
+            string message = Logic.Message(smth);
+
+            Assert.AreEqual("Последовательность: " + smth + " отрицательна!", message);
+        }
+    }
+}
